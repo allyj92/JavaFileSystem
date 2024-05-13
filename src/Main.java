f27492c2c4370e76cc8086be89661b7aca33d742
eunjaelim <imeunjae361@gmail.com>
2024-05-13 21:18:26 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.File;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("secret_message.txt");
+
+        if (file.exists()) {
+            System.out.println("That file exists! :O!");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            file.delete();
+        }else{
+            System.out.println("That file doesn't exist :(");
+        }
+    }
+}
